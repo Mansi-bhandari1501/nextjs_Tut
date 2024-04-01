3c743c9f1305df9b2e6a865fb9d60d437aa725e2
Mansi-bhandari1501 <mansi.bhandari1501@gmail.com>
2024-04-01 17:03:25 +0530

    first commit

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,3 @@
+export default function ForgotPassword(){
+    return <h1>Welcome ForgotPassword</h1>
+}
---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,11 @@
+export default function ProductList({params}:{
+    params:{productId: string};
+}){
+    return(
+        <>
+        <h1>Product Details {params.productId}</h1>
+        <h1>Product </h1>
+      
+        </>
+    )
+}
